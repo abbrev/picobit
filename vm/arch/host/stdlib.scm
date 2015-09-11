777926e19bcff0c7bac09157c8d784846fa62d50
Christopher Williams <abbrev@gmail.com>
2015-09-10 17:10:15 -0700

    Remove unnecessary lambdas

--- vm/arch/host/stdlib.scm
+++ vm/arch/host/stdlib.scm
@@ -1,47 +1,39 @@
 (define light adc)
 
-(define putchar
-  (lambda (c)
-    (#%putchar c 3)))
+(define (putchar c)
+    (#%putchar c 3))
 
-(define getchar
-  (lambda ()
+(define (getchar)
     (or (#%getchar-wait 0 3)
-        (getchar))))
+        (getchar)))
 
-(define getchar-wait
-  (lambda (duration)
-    (#%getchar-wait duration 3)))
+(define (getchar-wait duration)
+    (#%getchar-wait duration 3))
 
-(define sleep
-  (lambda (duration)
-    (#%sleep-aux (#%+ (clock) duration))))
+(define (sleep duration)
+    (#%sleep-aux (#%+ (clock) duration)))
 
-(define #%sleep-aux
-  (lambda (wake-up)
+(define (#%sleep-aux wake-up)
     (if (< (clock) wake-up)
         (#%sleep-aux wake-up)
-        #f)))
+        #f))
 
 
-(define led2-color
-  (lambda (state)
+(define (led2-color state)
     (if (eq? state 'red)
         (#%led2-color 1)
-        (#%led2-color 0))))
+        (#%led2-color 0)))
 
-(define display
-  (lambda (x)
+(define (display x)
     (if (string? x)
         (for-each putchar (string->list x))
-        (write x))))
+        (write x)))
 
 (define (newline) (#%putchar #\newline 3))
 
 (define (displayln x) (display x) (newline))
 
-(define write
-  (lambda (x)
+(define (write x)
     (cond ((string? x)
 	   (begin (#%putchar #\" 3)
 		  (display x)
@@ -57,11 +49,10 @@
 	  ((boolean? x)
 	   (display (if x "#t" "#f")))
 	  (else
-	   (display "#<object>")))))
+	   (display "#<object>"))))
 ;; TODO have vectors and co ?
 
-(define #%write-list
-  (lambda (lst)
+(define (#%write-list lst)
     (cond ((null? lst)
 	   (#%putchar #\) 3))
 	  ((pair? lst)
@@ -71,12 +62,11 @@
 	  (else
 	   (begin (display " . ")
 		  (write lst)
-		  (#%putchar #\) 3))))))
+		  (#%putchar #\) 3)))))
 
-(define pp
-  (lambda (x)
+(define (pp x)
     (write x)
-    (#%putchar #\newline 3)))
+    (#%putchar #\newline 3))
 
 (define current-time clock)
-(define time->seconds (lambda (t) (quotient t 100)))
+(define (time->seconds t) (quotient t 100))
