bdba3e0f7146cdd94ab230db54c3647d196b2ec7
Christopher Williams <abbrev@gmail.com>
2015-10-09 10:11:06 -0700

    Implement all c????r functions

--- compiler/library.scm
+++ compiler/library.scm
@@ -252,42 +252,34 @@
           rest
           (#%number->string-aux (quotient n 10) rest)))))
 
-(define caar
-  (lambda (p)
-    (car (car p))))
-(define cadr
-  (lambda (p)
-    (car (cdr p))))
-(define cdar
-  (lambda (p)
-    (cdr (car p))))
-(define cddr
-  (lambda (p)
-    (cdr (cdr p))))
-(define caaar
-  (lambda (p)
-    (car (car (car p)))))
-(define caadr
-  (lambda (p)
-    (car (car (cdr p)))))
-(define cadar
-  (lambda (p)
-    (car (cdr (car p)))))
-(define caddr
-  (lambda (p)
-    (car (cdr (cdr p)))))
-(define cdaar
-  (lambda (p)
-    (cdr (car (car p)))))
-(define cdadr
-  (lambda (p)
-    (cdr (car (cdr p)))))
-(define cddar
-  (lambda (p)
-    (cdr (cdr (car p)))))
-(define cdddr
-  (lambda (p)
-    (cdr (cdr (cdr p)))))
+(define (caar p) (car (car p)))
+(define (cadr p) (car (cdr p)))
+(define (cdar p) (cdr (car p)))
+(define (cddr p) (cdr (cdr p)))
+(define (caaar p) (car (car (car p))))
+(define (caadr p) (car (car (cdr p))))
+(define (cadar p) (car (cdr (car p))))
+(define (caddr p) (car (cdr (cdr p))))
+(define (cdaar p) (cdr (car (car p))))
+(define (cdadr p) (cdr (car (cdr p))))
+(define (cddar p) (cdr (cdr (car p))))
+(define (cdddr p) (cdr (cdr (cdr p))))
+(define (caaaar p) (car (car (car (car p)))))
+(define (caaadr p) (car (car (car (cdr p)))))
+(define (caadar p) (car (car (cdr (car p)))))
+(define (caaddr p) (car (car (cdr (cdr p)))))
+(define (cadaar p) (car (cdr (car (car p)))))
+(define (cadadr p) (car (cdr (car (cdr p)))))
+(define (caddar p) (car (cdr (cdr (car p)))))
+(define (cadddr p) (car (cdr (cdr (cdr p)))))
+(define (cdaaar p) (cdr (car (car (car p)))))
+(define (cdaadr p) (cdr (car (car (cdr p)))))
+(define (cdadar p) (cdr (car (cdr (car p)))))
+(define (cdaddr p) (cdr (car (cdr (cdr p)))))
+(define (cddaar p) (cdr (cdr (car (car p)))))
+(define (cddadr p) (cdr (cdr (car (cdr p)))))
+(define (cdddar p) (cdr (cdr (cdr (car p)))))
+(define (cddddr p) (cdr (cdr (cdr (cdr p)))))
 
 (define equal?
   (lambda (x y)
